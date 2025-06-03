f7129186f85d3452e2ca6cb89e9a9d1f8e970ba9
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-06-03 12:02:16 +0200

    Created main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# from auth.login import login
+from managers.system_admin_manager import system_admin_crud
+
+
+def main():
+    user = None
+    # while not user:
+    #     user = login()
+
+    while True:
+        # if user["role"] == "super_admin":
+        print("\n--- Super Admin Menu ---")
+        print("1. Manage System Administrators")
+        print("2. View Logs (komt binnenkort)")
+        print("3. Logout")
+
+        choice = input("Choose an option: ").strip()
+        if choice == "1":
+            system_admin_crud()
+        elif choice == "2":
+            print("Logs komt hierna.")
+        elif choice == "3":
+            print("Logged out.")
+            break
+        else:
+            print("Invalid choice.")
+
+
+if __name__ == "__main__":
+    main()
