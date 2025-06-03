d277b1a1ac1b35040357dae924402fef7aaa37c9
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-06-03 15:56:09 +0200

    Updated main.py to handle new crud

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 # from auth.login import login
 
 from managers.system_admin_manager import system_admin_crud
+from managers.service_engineer_manager import service_engineer_crud
 
 
 def main():
-    user = None
-    # while not user:
-    #     user = login()
+    # user = {"username": "super_admin", "role": "super_admin"}
+    user = {"username": "demo_admin", "role": "system_admin"}
 
     while True:
-        # if user["role"] == "super_admin":
-        print("\n--- Super Admin Menu ---")
-        print("1. Manage System Administrators")
-        print("2. View Logs (komt binnenkort)")
-        print("3. Logout")
+        if user["role"] == "super_admin":
+            print("\n--- Super Admin Menu ---")
+            print("1. Manage System Administrators")
+            print("2. View Logs (komt binnenkort)")
+            print("3. Logout")
+            choice = input("Choose an option: ").strip()
+            if choice == "1":
+                system_admin_crud()
+            elif choice == "2":
+                print("Logs komt hierna.")
+            elif choice == "3":
+                print("Logged out.")
+                break
+            else:
+                print("Invalid choice.")
 
-        choice = input("Choose an option: ").strip()
-        if choice == "1":
-            system_admin_crud()
-        elif choice == "2":
-            print("Logs komt hierna.")
-        elif choice == "3":
-            print("Logged out.")
-            break
-        else:
-            print("Invalid choice.")
+        elif user["role"] == "system_admin":
+            print("\n--- System Admin Menu ---")
+            print("1. Manage Service Engineers")
+            print("2. Logout")
+            choice = input("Choose option: ").strip()
+            if choice == "1":
+                service_engineer_crud()
+            elif choice == "2":
+                print("Logged out.")
+                break
+            else:
+                print("Invalid choice.")
 
 
 if __name__ == "__main__":
