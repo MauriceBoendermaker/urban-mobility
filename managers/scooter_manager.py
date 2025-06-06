f8ff440b6b8f11942307ef1333f14303e3a47c6c
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-06-06 11:30:47 +0200

    Made a start with scooters crud

---
+++ managers/scooter_manager.py
@@ -0,0 +1,19 @@
+from scooters.scooters import search_scooter, update_scooter_attributes
+
+
+def scooter_management_menu(role):
+    while True:
+        print("\n--- Scooter Management ---")
+        print("1. Search Scooter by Serial Number")
+        print("2. Update Scooter Info")
+        print("3. Back to Main Menu")
+
+        choice = input("Choose option: ").strip()
+        if choice == "1":
+            search_scooter()
+        elif choice == "2":
+            update_scooter_attributes(role)
+        elif choice == "3":
+            break
+        else:
+            print("Invalid option.")
