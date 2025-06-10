73487b2d8982e12d360186274209b0473adba530
mark-175 <152170112+mark-175@users.noreply.github.com>
2025-06-10 11:31:41 +0200

    Travellers CRUD completed

--- travellers/travellers.py
+++ travellers/travellers.py
@@ -33,9 +33,17 @@ def travellers_crud_menu():
         elif choice == "2":
             register_traveller_menu()
         elif choice == "3":
-            update_traveller()
+            travellers = show_all_travellers()
+            if travellers == None:
+                print("No travellers found!")
+            else:
+                update_travellers_menu(travellers)
         elif choice == "4":
-            delete_traveller()
+            travellers = show_all_travellers()
+            if travellers == None:
+                print("No travellers found!")
+            else:
+                delete_traveller_menu(travellers)
         elif choice == "5":
             break
         else:
@@ -44,7 +52,7 @@ def travellers_crud_menu():
 
 def show_all_travellers():
     travellers = cursor.execute(
-        "SELECT first_name_enc, last_name_enc, birthday, gender, street_name_enc, house_number_enc, zip_code_enc, city, email_enc, mobile_phone_enc, driving_license_enc, registration_date FROM travellers").fetchall()
+        "SELECT * FROM travellers").fetchall()
 
     return travellers if len(travellers) > 0 else None
 
@@ -129,12 +137,130 @@ def register_traveller(first_name, last_name, birthday, gender, street_name, hou
         print("An unexpected error occurred. Please contact support.")
 
 
-def update_traveller():
-    pass
+def update_travellers_menu(travellers):
+    list_travellers(travellers)
+    traveller_id = -1
+    while True:
+        input_id = input(
+            "Enter the ID of the traveller you want to update: ").strip()
+        try:
+            traveller_id = int(input_id)
+            id_exists = False
+            for traveller in travellers:
+                if traveller[0] == traveller_id:
+                    id_exists = True
+
+            if not id_exists:
+                print(f"The traveller with id {traveller_id} doesn't exist")
+            else:
+                break
+        except:
+            print("Invalid input")
+
+    first_name = get_valid_input(
+        "First name: ", validate_name, "First name is not valid")
+    last_name = get_valid_input(
+        "Last name: ", validate_name, "Last name is not valid")
+    birthday = get_valid_input(
+        "Birthday (YYYY-MM-DD): ", validate_iso_date, "Birthday format was not valid")
+    gender = get_valid_input(
+        "Gender (M/F): ", validate_gender, "Invalid input")
+    street_name = get_valid_input(
+        "Street name: ", validate_street_name, "Invalid street name")
+    house_number = get_valid_input(
+        "House number: ", validate_house_number, "Invalid house number")
+    zip_code = get_valid_input(
+        "Zip code: ", validate_zip_code, "Invalid zip code")
+    print("Choose the city from the list: ")
+    for c in CITIES:
+        print(c)
+    city_number = get_valid_input(
+        f"Enter the number of the city (1-{len(CITIES)}): ", validate_city_number, "Invalid input")
+
+    city = CITIES[int(city_number) - 1].split(" ")[1]
+
+    email_address = get_valid_input(
+        "Email address: ", validate_email_address, "Invalid email address")
+    mobile_phone = get_valid_input(
+        "Mobile phone: +31 6", validate_phone_number, "Invalid phone number")
+    driving_license_number = get_valid_input(
+        "Driving license number: ", validate_driving_license, "Invalid input")
+
+    mobile_phone = "+31 6" + mobile_phone
+
+    update_traveller(
+        traveller_id,
+        first_name,
+        last_name,
+        birthday,
+        gender,
+        street_name,
+        house_number,
+        zip_code,
+        city,
+        email_address,
+        mobile_phone,
+        driving_license_number
+    )
 
 
-def delete_traveller():
-    pass
+def update_traveller(id, first_name, last_name, birthday, gender, street_name, house_number, zip_code, city, email_address, mobile_phone, driving_license_number):
+    try:
+        cursor.execute("""
+            UPDATE travellers
+            SET first_name_enc=?, last_name_enc=?, birthday=?, gender=?, street_name_enc=?, house_number_enc=?, zip_code_enc=?, city=?, email_enc=?, mobile_phone_enc=?, driving_license_enc=?
+            WHERE traveller_id=?
+        """, (
+            deterministic_encrypt(first_name),
+            deterministic_encrypt(last_name),
+            deterministic_encrypt(birthday),
+            deterministic_encrypt(gender),
+            deterministic_encrypt(street_name),
+            deterministic_encrypt(house_number),
+            deterministic_encrypt(zip_code),
+            deterministic_encrypt(city),
+            deterministic_encrypt(email_address),
+            deterministic_encrypt(mobile_phone),
+            deterministic_encrypt(driving_license_number),
+            id
+        ))
+        conn.commit()
+        print("Traveller updated successfully.")
+    except Exception:
+        print("An error occurred while updating the traveller")
+
+
+def delete_traveller_menu(travellers):
+    list_travellers(travellers)
+    traveller_id = -1
+    while True:
+        input_id = input(
+            "Enter the ID of the traveller you want to update: ").strip()
+        try:
+            traveller_id = int(input_id)
+            id_exists = False
+            for traveller in travellers:
+                if traveller[0] == traveller_id:
+                    id_exists = True
+
+            if not id_exists:
+                print(f"The traveller with id {traveller_id} doesn't exist")
+            else:
+                break
+        except:
+            print("Invalid input")
+    delete_traveller(traveller_id)
+
+
+def delete_traveller(id):
+    try:
+
+        cursor.execute("""DELETE FROM travellers WHERE traveller_id = ?;
+        """, str(id))
+        print("traveller with id {id} has been succesfully deleted")
+    except Exception as e:
+        print(
+            f"An error has occured while deleting traveller with id :{id}", e)
 
 
 def list_travellers(travellers):
@@ -142,10 +268,9 @@ def list_travellers(travellers):
 
     for t in travellers:
 
-        first_name, last_name, birthday, gender, street_name, house_number, zip_code, city, email_address, mobile_phone, driving_license_number, registration_date = t
+        traveller_id, first_name, last_name, birthday, gender, street_name, house_number, zip_code, city, email_address, mobile_phone, driving_license_number, registration_date = t
         print(
-
-            f"""Full name: {deterministic_decrypt(first_name)} {deterministic_decrypt(last_name)}, """
+            f"""ID: {traveller_id}, Full name: {deterministic_decrypt(first_name)} {deterministic_decrypt(last_name)}, """
             f"""Birthday: {deterministic_decrypt(birthday)}, Gender: {"Male" if deterministic_decrypt(gender).upper() else "Female"}, """
             f"""Address: {deterministic_decrypt(street_name)} {deterministic_decrypt(house_number)}, {deterministic_decrypt(zip_code)} {deterministic_decrypt(city)}, """
             f"""Email: {deterministic_decrypt(email_address)}, Phone: {deterministic_decrypt(mobile_phone)}, """
