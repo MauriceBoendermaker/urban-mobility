18a759086324de88984280cd405db5a8c15c41ad
mark-175 <152170112+mark-175@users.noreply.github.com>
2025-06-20 22:33:43 +0200

    servicve engineer update own password check

--- src/menus/service_engineer_menu.py
+++ src/menus/service_engineer_menu.py
@@ -1,8 +1,9 @@
 from src.auth.auth_manager import update_own_password
 from src.managers.scooter_manager import scooter_management_menu
+from src.utils.sessions import get_user_id_from_session
 
 
-def service_engineer_menu(role):
+def service_engineer_menu(session_token):
     while True:
         print("\n--- Service Engineer Menu ---")
         print("1. Manage Scooters")
@@ -11,9 +12,10 @@ def service_engineer_menu(role):
         choice = input("Choose option: ").strip()
 
         if choice == "1":
-            scooter_management_menu(role)
+            scooter_management_menu(session_token)
         elif choice == "2":
-            update_own_password(role)
+            user_id = get_user_id_from_session(session_token)
+            update_own_password(user_id)
         elif choice == "3":
             print("Logged out.")
             break
