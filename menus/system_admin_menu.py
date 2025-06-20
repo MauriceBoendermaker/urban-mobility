690e5f44fc1f92a8cb17fea67d5a041fc27a9969
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-06-20 12:27:58 +0200

    Added scooter crud options for sys admin

--- menus/system_admin_menu.py
+++ menus/system_admin_menu.py
@@ -1,6 +1,7 @@
 from managers.service_engineer_manager import service_engineer_crud
 from utils.backup_manager import create_backup, restore_backup
 from managers.travellers_manager import travellers_crud_menu
+from managers.scooter_manager import scooter_management_menu
 
 
 def system_admin_menu(session_token):
@@ -8,9 +9,10 @@ def system_admin_menu(session_token):
         print("\n--- System Admin Menu ---")
         print("1. Manage Service Engineers")
         print("2. Manage Travellers")
-        print("3. Create Backup")
-        print("4. Restore Backup")
-        print("5. Logout")
+        print("3. Manage Scooters")
+        print("4. Create Backup")
+        print("5. Restore Backup")
+        print("6. Logout")
         choice = input("Choose option: ").strip()
 
         if choice == "1":
@@ -18,10 +20,12 @@ def system_admin_menu(session_token):
         elif choice == "2":
             travellers_crud_menu()
         elif choice == "3":
-            create_backup(session_token)
+            scooter_management_menu("system_admin")
         elif choice == "4":
-            restore_backup(session_token)
+            create_backup(session_token)
         elif choice == "5":
+            restore_backup(session_token)
+        elif choice == "6":
             print("Logging out.")
             break
         else:
