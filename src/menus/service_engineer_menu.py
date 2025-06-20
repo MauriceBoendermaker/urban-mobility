e77e8bca12eac3c8fb93b5735b3f6b91864b55ea
mark-175 <152170112+mark-175@users.noreply.github.com>
2025-06-20 22:49:18 +0200

    delete own account

--- src/menus/service_engineer_menu.py
+++ src/menus/service_engineer_menu.py
@@ -1,4 +1,4 @@
-from src.auth.auth_manager import update_own_password, delete_own_account
+from src.auth.auth_manager import update_own_password
 from src.managers.scooter_manager import scooter_management_menu
 from src.utils.sessions import get_user_id_from_session
 
@@ -8,23 +8,16 @@ def service_engineer_menu(session_token):
         print("\n--- Service Engineer Menu ---")
         print("1. Manage Scooters")
         print("2. Change Own Password")
-        print("3. Delete your account")
-
-        print("4. Logout")
+        print("3. Logout")
         choice = input("Choose option: ").strip()
-
-        user_id = get_user_id_from_session(session_token)
 
         if choice == "1":
             scooter_management_menu(session_token)
         elif choice == "2":
+            user_id = get_user_id_from_session(session_token)
             update_own_password(user_id)
-        elif choice == "3":
-            if delete_own_account(user_id):
-                print("Logging out.")
-                break
 
-        elif choice == "4":
+        elif choice == "3":
             print("Logged out.")
             break
         else:
